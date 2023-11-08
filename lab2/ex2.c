becf78f2f8f7bf0c0c9cd2568dc3ba2248149193
Apostol-Alin <alin-constantin.apostol@s.unibuc.ro>
2023-11-08 20:09:27 +0200

    changes lab2

--- lab2/ex2.c
+++ lab2/ex2.c
@@ -9,23 +9,32 @@ int main(int argc, char* argv[]){
 	int file_open = open(argv[1], O_CREAT | O_RDONLY);
 	struct stat sb;
 	if(stat(argv[1], &sb)){
-		perror("error");
+		perror("Stat error");
 		return errno;
 	}
-	if(file_open < 0)
+	if(file_open < 0){
+		perror("Open error");
 		return errno;
+	}
 	off_t size = sb.st_size;
 	char* buff = malloc(size);
 	ssize_t how_much_read = read(file_open, buff, size);
-	if(how_much_read < 0)
+	if(how_much_read != size){
+		perror("Error while reading from file");
 		return errno;
+	}
 	close(file_open);
-	file_open = open(argv[2], O_CREAT | O_WRONLY);
-	if(file_open < 0)
+	file_open = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC);
+	if(file_open < 0){
+		perror("Open error");
 		return errno;
+	}
 	ssize_t how_much_written = write(file_open, buff, size);
-	if(how_much_written != size)
+	if(how_much_written != size){
+		perror("Error while writing in file");
 		return errno;
+	}
+	close(file_open);
 	free(buff);
 	return 0;
 }
