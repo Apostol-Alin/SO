25754bb3939eba3f811bec6cd17e1aa1e68efe8c
Apostol-Alin <alin-constantin.apostol@s.unibuc.ro>
2023-12-04 20:25:30 +0200

    added comments to lab5

--- lab5/ex1.c
+++ lab5/ex1.c
@@ -25,14 +25,20 @@ int main(int argc, char* argv[]){
 	int shm_fd;
 	int i = 1;
 	shm_fd = shm_open(shm_name, O_CREAT|O_RDWR, S_IRUSR|S_IWUSR);
+	// flag-ul O_CREAT : daca nu exista obiectul de memorie shm_name
+	// atunci este creat
+	// O_RDWR sunt drepturile de citire scriere
+	// functia returneaza un descriptor
 	if (shm_fd < 0){
 		perror(NULL);
 		return errno;
 	}
 	size_t shm_size = argc * 4096;
 	if(ftruncate(shm_fd, shm_size) == -1){
+		// aici functia scurteaza sau mareste obiectul de la shm_fd
+		// la dimensiunea shm_size
 		perror(NULL);
-		shm_unlink(shm_name);
+		shm_unlink(shm_name); // stergem obiectul de memorie
 		return errno;
 	}
 	printf("Starting parent: %d\n",getpid());
@@ -42,9 +48,17 @@ int main(int argc, char* argv[]){
 			return errno;
 		else if (pid == 0){
 			int *shm_ptr = mmap(0,4096,PROT_WRITE, MAP_SHARED, shm_fd,(i-1)*4096);
+			// primul argument al functiei este adresa
+			// la care sa fie incarcata in proces memoria alocata
+			// apoi dimensiunea
+			// urmeaza drepturile de acces (in cazul nostru de scrierer)
+			// MAP_SHARED este un flag care indica faptul ca modificarile facute
+			// de proces sa fie vizibile si in celelalte procese
+			// descriptorul obtinul prin shm_open
+			// si offset-ul
 			collatz(atoi(argv[i]), shm_ptr, 0);
 			printf("Done Parent %d Me %d\n",getppid(),getpid());
-			munmap(shm_ptr,4096);
+			munmap(shm_ptr,4096); //dezalocam memoria de la adresa indicata de pointerul shm_ptr
 			return 0;
 		}
 		i++;
@@ -58,6 +72,7 @@ int main(int argc, char* argv[]){
 	while(argv[i]){
 		printf("%d :",atoi(argv[i]));
 		int *shm_ptr = mmap(0,4096, PROT_READ, MAP_SHARED, shm_fd, (i-1) * 4096);
+		// aici avem drepturi de citire in procesul parinte
 		int j = 0;
 		while(shm_ptr[j]){
 			printf("%d ",shm_ptr[j]);
