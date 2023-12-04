cca170730584ac3ef47ed2e085873938c00b66fa
Apostol-Alin <alin-constantin.apostol@s.unibuc.ro>
2023-12-04 19:50:32 +0200

    added comments to lab2

--- lab2/ex1.c
+++ lab2/ex1.c
@@ -3,6 +3,11 @@
 
 int main(){
 	ssize_t size = write(1,"Hello, World!\n",14);
+	//primul argument e file descriptorul
+	//in cazul nostru 1 (standard output)
+	//al doilea este stringul de scris
+	//al treilea este dimensiunea
+	//returneaza numarul de bytes scris
 	if (size < 0){
 		perror("error");
 		return errno;
--- lab2/ex2.c
+++ lab2/ex2.c
@@ -7,6 +7,9 @@
 
 int main(int argc, char* argv[]){
 	int file_open = open(argv[1], O_CREAT | O_RDONLY);
+	// functie open returneaza file_descriptor-ul asociat fisierului
+	// flag-ul O_CREAT spune ca daca fisierul nu exista, sa se creeze
+	// flag-ul O_RDONLY restrictioneaza doar spre citire din fisier
 	struct stat sb;
 	if(stat(argv[1], &sb)){
 		perror("Stat error");
@@ -17,24 +20,38 @@ int main(int argc, char* argv[]){
 		return errno;
 	}
 	off_t size = sb.st_size;
+	//ne luam cati bytes avem nevoie
 	char* buff = malloc(size);
+	//alocam dinamic un buffer
 	ssize_t how_much_read = read(file_open, buff, size);
+	//citim din fisier
+	//functia intoarce cati bytes s-au citit
 	if(how_much_read != size){
 		perror("Error while reading from file");
 		return errno;
 	}
 	close(file_open);
+	//inchidem fisierul cu fd-ul file_open
 	file_open = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC);
+	//deschidem fisierul destinatie
+	// O_WRONLY restrictioneaza doar spre scriere
+	// O_TRUNC : daca a fost deschis fisierul cu drept de scriere
+	// si fisierul exista deja il trunchiaza la lungime 0 BYTES
 	if(file_open < 0){
 		perror("Open error");
 		return errno;
 	}
 	ssize_t how_much_written = write(file_open, buff, size);
+	//scriem in fisier si vedem cat am scris
 	if(how_much_written != size){
 		perror("Error while writing in file");
+		//daca am scris mai putin decat am avut in fiserul sursa
+		//s-a produs o eroare
 		return errno;
 	}
 	close(file_open);
+	//inchidem fisierul
 	free(buff);
+	//dezalocam memoria
 	return 0;
 }
