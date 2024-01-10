aa7421e3d6f3cfa2784e86dbd24b89ad47564b27
Apostol-Alin <alin-constantin.apostol@s.unibuc.ro>
2024-01-10 08:00:18 +0200

    proiect final

--- test/test.c
+++ test/test.c
@@ -16,7 +16,7 @@
 #include <signal.h>
 #include <semaphore.h>
 
-#define PROCESS_SHARED 1
+#define PROCESS_SHARED 1 /* pentru ca fiecare proces sa aiba acces la semafor */
 #define TIMP_PENTRU_FREZAT 1
 #define ADEVARAT 1
 #define SEATS 5
@@ -27,14 +27,12 @@ struct Monitor {
 	sem_t barbers;
 	sem_t customers;
 	sem_t mutex;
-	//sem_t counter_mutex;
 
 	int freeSeats;
 	pid_t customerPids[SEATS];
 	pid_t barberPids[SEATS];
 	int nextCustomer;
 	int nextSeat;
-	//int counter;
 };
 
 int init_Monitor(struct Monitor *monitor) {
@@ -45,21 +43,19 @@ int init_Monitor(struct Monitor *monitor) {
 		perror ( NULL );
 		return errno;
     	}
+    	// Cu semaforul penru customers simulam o variabila conditionala
+	// Daca un barber face post (adica acesta cheama un customer sa fie tuns), atunci se trezeste un customer
+	// Altfel, acesta asteapta sa fie tuns pana in momentul in care il cheama un frizer
     	if (sem_init(&(monitor->customers), PROCESS_SHARED, 0)) {
 		perror ( NULL );
 		return errno;
     	}
+    	// Simulam un mutex folosind cu semafor
     	if (sem_init(&(monitor->mutex), PROCESS_SHARED, 1)) {
 		perror ( NULL );
 		return errno;
     	}
-    	//if (sem_init(&(monitor->counter_mutex), PROCESS_SHARED, 1)) {
-		//perror ( NULL );
-		//return errno;
-    	//}
 
-
-	//monitor->counter = 1;
     	monitor->freeSeats = SEATS;
     	monitor->nextCustomer = monitor->nextSeat = 0;
 
@@ -67,18 +63,9 @@ int init_Monitor(struct Monitor *monitor) {
 }
 
 void destroy_Monitor (struct Monitor *monitor) {
-	/*for(int i = 0; i < BARBERS; i++)
-		sem_post(&(monitor->counter_mutex));
-	for(int i = 0; i < CUSTOMERS; i++)
-		sem_post(&(monitor->customers));
-	for(int i = 0; i < BARBERS; i++)
-		sem_post(&(monitor->barbers));
-	for(int i = 0; i < BARBERS; i++)
-		sem_post(&(monitor->mutex));*/
 	sem_destroy(&(monitor->barbers));
 	sem_destroy(&(monitor->customers));
 	sem_destroy(&(monitor->mutex));
-	//sem_destroy(&(monitor->counter_mutex));
 }
 
 
@@ -88,33 +75,25 @@ void barber(struct Monitor *monitor) {
     	
 	printf("Barber %d joins shop\n", getpid());
 	while(ADEVARAT){
+	
 		printf("Barber %d goes to sleep\n", getpid());
-		/*sem_wait(&(monitor->counter_mutex));
-		if(monitor->counter >= CUSTOMERS){
-			sem_post(&(monitor->counter_mutex));
-			break;
-		}
-		else
-			sem_post(&(monitor->counter_mutex));*/
-		sem_wait(&(monitor->barbers)); // Barber goes to barber queue
-		sem_wait(&(monitor->mutex)); // Lock the mutex to secure the seats
-		myNext = monitor->nextCustomer;
-		monitor->nextCustomer = ((monitor->nextCustomer) + 1) % SEATS;
-		customerPid = monitor->customerPids[myNext]; // Get the Customer's PID
-		monitor->barberPids[myNext] = getpid(); // Give my PID to the Customer
-		sem_post(&(monitor->mutex)); // Unlock the mutex to leave access to seats
-		sem_post(&(monitor->customers)); // Call the next costumer for his freza
+		struct timespec timeout;
+    		clock_gettime(CLOCK_REALTIME, &timeout);
+    		timeout.tv_sec += 4;
+    		// Barber se va pune la coada (doarme, pana cand il trezeste un client)
+    		// Daca ajunge sa stea peste program (nu mai sunt clienti), atunci procesul se incheie
+		if (sem_timedwait(&(monitor->barbers), &timeout) == -1)
+			return;
+		sem_wait(&(monitor->mutex)); // Asteptam acces la mutex pentru a avea acces la variabilele din monitor
+		myNext = monitor->nextCustomer; // Luam urmatorul Customer
+		monitor->nextCustomer = ((monitor->nextCustomer) + 1) % SEATS; // Setam urmatorul Customer care sa fie servit
+		customerPid = monitor->customerPids[myNext]; // Luam PID-ul Customer
+		monitor->barberPids[myNext] = getpid(); // Punem PID-ul pentru Customer
+		sem_post(&(monitor->mutex)); // Deblocam mutex-ul
+		sem_post(&(monitor->customers)); // Chemam urmatorul Customer pentru frezat
 		printf("Barber %d is serving the customer %d\n", getpid(), customerPid);
 		sleep(TIMP_PENTRU_FREZAT);
 		printf("Barber %d finished cutting the hair for customer %d\n", getpid(), customerPid);
-		/*sem_wait(&(monitor->counter_mutex));
-		monitor->counter = monitor->counter + 1;
-		if(monitor->counter >= CUSTOMERS){
-			sem_post(&(monitor->counter_mutex));
-			break;
-		}
-		else
-			sem_post(&(monitor->counter_mutex));*/
 
 	}
 }
@@ -122,30 +101,27 @@ void barber(struct Monitor *monitor) {
 void customer(struct Monitor *monitor) {
     int mySeat;
     pid_t barberPid;
-    sem_wait(&(monitor->mutex));
+    sem_wait(&(monitor->mutex)); // Asteptam acces la mutex pentru a avea acces la variabilele din monitor
     printf("Customer %d enters shop\n", getpid());
-    if((monitor->freeSeats) > 0){
-        --(monitor->freeSeats);
-        printf("Customer %d sits in waiting room.\n", getpid());
-        mySeat = monitor->nextSeat;
-        monitor->nextSeat = ((monitor->nextSeat) + 1) % SEATS;
-        monitor->customerPids[mySeat] = getpid();
-        sem_post(&(monitor->mutex));
-        sem_post(&(monitor->barbers));
-        sem_wait(&(monitor->customers));
-        sem_wait(&(monitor->mutex));
-        barberPid = monitor->barberPids[mySeat];
-        (monitor->freeSeats)++;
-        sem_post(&(monitor->mutex));
+    if((monitor->freeSeats) > 0){ // Verificam daca sunt locuri libere
+        --(monitor->freeSeats); // Scadem numarul de locuri libere
+        printf("Customer %d sits in waiting room.\n", getpid()); 
+        mySeat = monitor->nextSeat; // Luam locul pe care trebuie sa ne asezam
+        monitor->nextSeat = ((monitor->nextSeat) + 1) % SEATS; // Setam urmatorul loc liber
+        monitor->customerPids[mySeat] = getpid(); // Punem PID-ul pentru Barber
+        sem_post(&(monitor->mutex)); // Deblocam mutex-ul
+        sem_post(&(monitor->barbers)); // Notificam un Barber ca trebuie sa serveasca un Customer
+        sem_wait(&(monitor->customers)); // Asteptam ca un Barber sa ne cheme ca putem fi serviti
+        sem_wait(&(monitor->mutex)); // Asteptam acces la mutex pentru a avea acces la variabilele din monitor
+        barberPid = monitor->barberPids[mySeat]; // Luam PID-ul Barber
+        (monitor->freeSeats)++; // Incrementam numarul de locuri de asteptare libere
+        sem_post(&(monitor->mutex)); // Deblocam mutex-ul
         printf("Customer %d is having hair cut by barber %d.\n", getpid(), barberPid);
         
     }
     else{
-        sem_post(&(monitor->mutex));
-        /*sem_wait(&(monitor->counter_mutex));
-   	monitor->counter = monitor->counter + 1;
-   	sem_post(&(monitor->counter_mutex));*/
-        printf("Customer %d finds no seat and leaves.\n",getpid());
+        sem_post(&(monitor->mutex)); // Deblocam mutex-ul
+        printf("Customer %d finds no seat and leaves.\n",getpid()); // Nu au fost gasite locuri libere
     
     }
    
@@ -230,17 +206,14 @@ int main () {
 	for (int i = 1; i <= CUSTOMERS; i++)
 		if (pids[BARBERS + i] != 0)
 			wait(NULL);
-			
-	for (int i = 1; i <= BARBERS; i++)
-		if (pids[i] != 0){
-			kill(pids[i], SIGTERM);
-			waitpid(pids[i], NULL, 0); 
-		}
+	sleep(3);
+	for (int i=1; i<=BARBERS; i++)
+		if (pids[i] != 0)
+			wait(NULL);
 
 	destroy_Monitor(shm_ptr_init);
 	munmap(shm_ptr_init, sizeof(struct Monitor));
 	shm_unlink(shm_name);
-
+	
 	exit(EXIT_SUCCESS);
-	//return 0;
 }
